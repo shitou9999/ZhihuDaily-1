1460a1c86a51bb4237d3f7b6dd9f323d84451111
18865091603 <725412890@qq.com>
2018-01-15 14:57:18 +0800

    kotlin

--- roomlearn/src/main/java/com/example/netty/roomlearn/MainActivity.java
+++ roomlearn/src/main/java/com/example/netty/roomlearn/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private AppDatabase mAppdataBase;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
